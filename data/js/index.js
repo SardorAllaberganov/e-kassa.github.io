c5499812b193df2fd4dd5ca292525a83b97584b0
SardorAllaberganov <sardorallaberganov96@gmail.com>
2019-04-22 14:41:46 +0500

    22 apr 2019

--- data/js/index.js
+++ data/js/index.js
@@ -2,35 +2,84 @@ var element = document.getElementById("sidebar");
 var coll = document.querySelectorAll(".collapsible");
 var content = document.querySelectorAll(".content");
 var i;
-const openMenu = () => {
-  element.classList.toggle("show");
-  for(i = 0; i < content.length; i++){
-    content[i].style.maxHeight = null;
-    coll[i].classList.remove("active");
+
+// $(".set").on("hover", function(){
+//   if($(this).children().children().nodeName == "SPAN"){
+//   }
+// });
+
+
+
+$(".set").hover(
+  function() {
+    var pos = $(this).position();
+
+    // console.log($(this).find('.content').text());
+
+    $(".title-onHover").css({
+      position: "absolute",
+      top: pos.top + 64 + "px",
+      left: pos.left + 60 + "px"
+    }).fadeIn();
+    $(".title-onHover").text($(this).find('span').text());
+    // var htmlString = $(this).html();
+    // $(this).text( htmlString );
+    // if($(this).find())
+    // if($(".set").children().children().nodeName == "SPAN"){
+    //   console.log("true");
+    // }
+  }, function() {
+    $(".title-onHover").css("display", "none");
+    $(".title-onHover").innerHTML = $(this).text();
+    
+    // $( this ).find( "span:last" ).remove();
   }
-}
+);
+// $('.set').click(function(){
+//   if(!element.classList.contains("show")){
+//     document.querySelector(".content").classList.add("show");
+//     console.log("show");
+//   }
+// });
 
+$('#menu').click(function(){
+  element.classList.toggle("show");
+  $(".set > a").removeClass("active1");
+  $(".content").slideUp(200);
+  $(".set > a i").removeClass("sort-down1").addClass("sort-up1");
+});
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    var content = this.nextElementSibling;
-    if(element.classList.contains('show')){
-      this.classList.toggle("active");
-      if (content.style.maxHeight){
-        content.style.maxHeight = null;
-        this.style.backgroundColor = "";
-      } else {
-        this.style.backgroundColor = "#00701A";
-        content.style.maxHeight = null;
-        content.style.maxHeight = content.scrollHeight + "px";
-      } 
+// Close the dropdown if the user clicks outside of it
+$(window).click(function(event){
+  if (!event.target.matches('#menu') && !event.target.matches('.set *')) {
+    if (element.classList.contains('show')) {
+      element.classList.remove('show');
+      $(".set > a").removeClass("active1");
+      $(".content").slideUp(200);
+      $(".set > a i").removeClass("sort-down1").addClass("sort-up1");
     }
-    else{
-      if (content.style.maxHeight){
+  }
+});
+
+
+$(document).ready(function() {
+  $(".set > a").on("click", function() {
+    if(element.classList.contains('show')){
+      if ($(this).hasClass("active1")) { 
+        $(this).removeClass("active1");
+        $(this).siblings(".content").slideUp(200);
+        $(".set > a i").removeClass("sort-down1").addClass("sort-up1");
+      } else {
+        $(".set > a i").removeClass("sort-down1").addClass("sort-up1");
+        $(this).find("i").removeClass("sort-up1").addClass("sort-down1");
+        $(".set > a").removeClass("active1");
+        $(this).addClass("active1");
+        $(".content").slideUp(200);
+        $(this).siblings(".content").slideDown(200);
       }
     }
   });
-}
+});
 
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show1");
@@ -49,26 +98,3 @@ window.onclick = function(event) {
     }
   }
 }
-
-// if(!element.classList.contains('show')){
-//   document.addEventListener('DOMContentLoaded', function() {
-
-//     const selector = '.buttons';
-//     const elems = Array.from(document.querySelectorAll(selector));
-//     const navigation = document.querySelector(".side-menu");
-
-//     function makeActive(evt) {
-//       const target = evt.target;
-
-//        if (!target || !target.matches(selector)) {
-//          return;
-//        }
-
-//       elems.forEach(elem => elem.classList.remove('active'));
-//       evt.target.classList.add('active');
-//     };
-
-//     navigation.addEventListener('mousedown', makeActive);
-
-//   });
-// }
